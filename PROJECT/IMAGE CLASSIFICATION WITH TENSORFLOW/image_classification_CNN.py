8dfb9b7658dc63246d759278ef40669a2d430355
Bibhakar Paul <87580111+BIB-HACKER@users.noreply.github.com>
2022-07-22 19:58:48 +0530

    Add files via upload

--- PROJECT/IMAGE CLASSIFICATION WITH TENSORFLOW/image_classification_CNN.py
+++ PROJECT/IMAGE CLASSIFICATION WITH TENSORFLOW/image_classification_CNN.py
@@ -1,3 +1,4 @@
+from tensorflow import keras 
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
@@ -8,27 +9,28 @@ from keras.layers import Dense
 classifier = Sequential()
  
 # Step 1 - Convolution
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
- 
+classifier.add(Convolution2D(32, 3, 3, 
+        input_shape = (64, 64, 3), activation = 'relu'))
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
  
 # Adding a second convolutional layer
 classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
-
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
  
 # Step 3 - Flattening
 classifier.add(Flatten())
  
 # Step 4 - Full connection
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(1, activation = 'relu'))
+classifier.add(Dense(1, activation = 'sigmoid'))
  
 # Compiling the CNN
-classifier.compile(optimizer ='adam', loss ='binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', 
+                   loss = 'binary_crossentropy',
+                   metrics = ['accuracy'])
 
-##################FITTING IMAGE TO CNN####################
+####FITTING IMAGE TO CNN#################################
 from keras.preprocessing.image import ImageDataGenerator
  
 train_datagen = ImageDataGenerator(rescale = 1./255,
@@ -50,23 +52,27 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = (8000/32),
-                         epochs = 25,
+                         steps_per_epoch = (1792/32),
+                         epochs = 5,
                          validation_data = test_set,
                          validation_steps = (2000/32))
+
+
+class_labels = {v: k for k, v in training_set.class_indices.items()}
+print(class_labels)
 ######Predicting the Image############
 from skimage.io import imread
 from skimage.transform import resize
 import numpy as np
  
-class_labels = {v: k for k, v in training_set.class_indices.items()} 
-print(class_labels)
+
 img = imread('pexels-photo-257540.jpeg') 
 img = resize(img,(64,64)) 
 img = np.expand_dims(img,axis=0) 
-prediction = classifier.predict_classes(img)
-#print(prediction) 
+#prediction = classifier.predict_classes(img)
+prediction = (classifier.predict(img) > 0.5).astype("int32")
+#print(prediction)
 if prediction[0][0]==1:
     print("It is a dog")
-elif prediction[0][0]==0: 
-    print("It is a cat") 
+elif prediction[0][0]==0:
+    print("It is a cat")
